ca2e9f4415d6c4acc49615bb840c1f054174843a
ambarcacciola <acacciola@mail.austral.edu.ar>
2025-06-26 22:29:24 -0300

    Update lists.py

--- lists.py
+++ lists.py
@@ -1,20 +1,34 @@
-# Replace the "ANSWER HERE" with your answer
+def remove_elements(lista):
+  nueva_lista = lista[:]
+  for i in [5, 4, 0]:
+   if len(nueva_lista) > i:
+     del nueva_lista[i]
+  return nueva_lista
 
-def remove_elements(list_to_remove_elements):
-    return "ANSWER HERE"  # Remove this line and implement
+def add_elements(lista):
+  agregados = lista[:]
+  agregados.append("Yellow")
+  agregados.insert(0, "Pink")
+  return agregados
 
+def is_empty(lista):
+  if lista == []:
+    return True
+  else:
+    return False
 
-def add_elements(list_to_add_elements):
-    return "ANSWER HERE"  # Remove this line and implement
+def check_lists(lista1, lista2):
+  if len(lista1) and len(lista2) > 3:
+    if lista1[2] == lista2[2]:
+      return True
 
+  else: 
+    return False
 
-def is_empty(list_to_check):
-    return "ANSWER HERE"  # Remove this line and implement
-
-
-def check_lists(list_to_compare1, list_to_compare2):
-    return "ANSWER HERE"  # Remove this line and implement
-
-
-def list_of_lists(list_of_lists_to_modify):
-    return "ANSWER HERE"  # Remove this line and implement
+def list_of_lists(lista):
+  nueva_lista = [
+      lista[0][:2],    
+      lista[1][0:4],    
+      lista[2][-2:]     
+  ]
+  return nueva_lista
